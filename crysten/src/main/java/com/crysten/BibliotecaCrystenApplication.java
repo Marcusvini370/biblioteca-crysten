3f997bbfeb54614b99a68884ad7e5d52dffda91c
Marcus Vinicius <marcusvini370@gmail.com>
2023-01-03 19:27:37 -0300

    Adicionando configurações iniciais do projeto

---
+++ crysten/src/main/java/com/crysten/BibliotecaCrystenApplication.java
@@ -0,0 +1,13 @@
+package com.crysten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaCrystenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaCrystenApplication.class, args);
+	}
+
+}
---
+++ crysten/src/test/java/com/crysten/BibliotecaCrystenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crysten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaCrystenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
