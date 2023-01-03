293875b48eaaec768ed09f49840d8a59101e3370
Marcus Vinicius <marcusvini370@gmail.com>
2023-01-03 20:46:49 -0300

    Adicionando os repositórios dos modelos

---
+++ crysten/src/main/java/com/crysten/domain/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.crysten.domain.repository;
+
+import com.crysten.domain.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+}
