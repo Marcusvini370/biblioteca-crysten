70f6a8d5518123e8bf00307e5a6f80d777f048eb
Marcus Vinicius <marcusvini370@gmail.com>
2023-01-03 21:12:49 -0300

    Adicionando migrações do bd com o flyway

---
+++ crysten/src/main/resources/db/migration/V002__criacao-tabela-cliente.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `cliente` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `bairro` varchar(255) DEFAULT NULL,
+  `cep` varchar(10) DEFAULT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `localidade` varchar(255) DEFAULT NULL,
+  `logradouro` varchar(255) DEFAULT NULL,
+  `uf` varchar(3) DEFAULT NULL,
+  `nome_completo` varchar(255) DEFAULT NULL,
+  `cpf` varchar(14) DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
